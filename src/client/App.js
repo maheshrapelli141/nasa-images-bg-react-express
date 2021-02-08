da458a42c1241af8e12e076074a6e5262d51b254
maheshrapelli141 <maheshrapelli141@gmail.com>
2021-02-08 12:18:48 +0530

    landing page modified according to given template

--- src/client/App.js
+++ src/client/App.js
@@ -6,7 +6,6 @@ export default class App extends Component {
   state = {
     date: null,
     nasaData: {},
-    readFull: false,
     isLoading: false
   };
 
@@ -38,60 +37,24 @@ export default class App extends Component {
     this.fetchNasa();
   }
 
-  toggleReadFull = () => this.setState({ readFull: !this.state.readFull });
-
   render() {
-    const { nasaData,isLoading,readFull } = this.state;
+    const { nasaData,isLoading } = this.state;
     return (
-      <div style={{position: 'relative',width:'100vw',height: '100vh',color:'#fff',backgroundColor: '#000',backgroundImage: `url(${nasaData.url})`,backgroundSize:'cover',backgroundRepeat:'no-repeat'}}>
-        <div className="bottom-section" style={{textAlign:'center',position: 'absolute',bottom:'10px',left:'50%',transform:'translate(-50%,-50%)'}}>
-          {isLoading ? 
-          'Loading...'
+      <div className="wrapper">
+        <h1>ACME</h1><hr />
+        <span>
+          <input type="date" onChange={this.handleOnChange} className="right"/>
+        </span>
+        {isLoading ?
+        'Loading...'
         :
         <>
-          <h2>{nasaData?.title}</h2>
-          <small>
-            <b>Date: {nasaData?.date}</b> |{' '}
-          <span>Change Date: <input type="date" onChange={this.handleOnChange}/></span></small>
-          <p>
-            {nasaData?.explanation?.substr(0,readFull ? nasaData?.explanation.length-1 : nasaData?.explanation.indexOf('.') )}
-            {' '}<a href="#" onClick={this.toggleReadFull}>show {readFull ? 'less' : 'more'}</a>
-          </p>
+        <img src={nasaData.url} height="100%" width="100%" className="banner-image"/>
+        <p>{nasaData.explanation}</p>
         </>
         }
-        </div>
+        
       </div>
-    );
-    // return (
-    //   <div className="wrapper container">
-    //     <h1>ACME</h1>
-    //     <div className="row">
-    //       <div className="col-sm-4">
-    //         <label>Select date to change details: </label>
-    //         <input type="date" onChange={this.handleOnChange} className="form-control" value={this.formatDate(this.state.date)}/>
-    //       </div>
-          
-    //     </div>
-    //     <hr />
-    //       <br />
-    //     {isLoading ?
-    //   'Loading...'  
-    //   :
-    //   <div className="row">
-    //     <div className="col-sm-6">
-    //       <img src={nasaData?.url} className="banner-image rounded img-thumbnail"/>
-    //     </div>
-    //     <div className="col-sm-6">
-        
-        
-    //       <h4>{nasaData?.title}</h4>
-    //       <small>{nasaData?.date}</small>
-    //       <p>{nasaData?.explanation}</p>  
-    //     </div>
-        
-    //   </div>
-    //   }
-    //   </div>
-    // );
+    )
   }
 }
